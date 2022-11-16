f32edb9f0b6dd60fcb2bd6c851f3a137d8c7a35e
GCSOliveira <116929616+GCSOliveira@users.noreply.github.com>
2022-11-16 13:28:56 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace fitro
+{
+    partial class Amplifier
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Amplifier));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.list_r1 = new System.Windows.Forms.ListBox();
+            this.list_r2 = new System.Windows.Forms.ListBox();
+            this.list_r3 = new System.Windows.Forms.ListBox();
+            this.list_c2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.amp_rate_in = new System.Windows.Forms.TextBox();
+            this.capacitance_in = new System.Windows.Forms.TextBox();
+            this.frequency_in = new System.Windows.Forms.TextBox();
+            this.gain_in = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::fitro.Properties.Resources.res01;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 142);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // list_r1
+            // 
+            this.list_r1.FormattingEnabled = true;
+            this.list_r1.Location = new System.Drawing.Point(12, 254);
+            this.list_r1.Name = "list_r1";
+            this.list_r1.Size = new System.Drawing.Size(120, 95);
+            this.list_r1.TabIndex = 1;
+            // 
+            // list_r2
+            // 
+            this.list_r2.FormattingEnabled = true;
+            this.list_r2.Location = new System.Drawing.Point(138, 254);
+            this.list_r2.Name = "list_r2";
+            this.list_r2.Size = new System.Drawing.Size(120, 95);
+            this.list_r2.TabIndex = 2;
+            // 
+            // list_r3
+            // 
+            this.list_r3.FormattingEnabled = true;
+            this.list_r3.Location = new System.Drawing.Point(264, 254);
+            this.list_r3.Name = "list_r3";
+            this.list_r3.Size = new System.Drawing.Size(120, 95);
+            this.list_r3.TabIndex = 3;
+            // 
+            // list_c2
+            // 
+            this.list_c2.FormattingEnabled = true;
+            this.list_c2.Location = new System.Drawing.Point(390, 254);
+            this.list_c2.Name = "list_c2";
+            this.list_c2.Size = new System.Drawing.Size(120, 95);
+            this.list_c2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(35, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.calcular);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(151, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 33);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clean";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Clean);
+            // 
+            // amp_rate_in
+            // 
+            this.amp_rate_in.Location = new System.Drawing.Point(410, 38);
+            this.amp_rate_in.Name = "amp_rate_in";
+            this.amp_rate_in.Size = new System.Drawing.Size(100, 20);
+            this.amp_rate_in.TabIndex = 7;
+            this.amp_rate_in.TextChanged += new System.EventHandler(this.validation);
+            // 
+            // capacitance_in
+            // 
+            this.capacitance_in.Location = new System.Drawing.Point(410, 77);
+            this.capacitance_in.Name = "capacitance_in";
+            this.capacitance_in.Size = new System.Drawing.Size(100, 20);
+            this.capacitance_in.TabIndex = 8;
+            this.capacitance_in.TextChanged += new System.EventHandler(this.validation);
+            // 
+            // frequency_in
+            // 
+            this.frequency_in.Location = new System.Drawing.Point(410, 116);
+            this.frequency_in.Name = "frequency_in";
+            this.frequency_in.Size = new System.Drawing.Size(100, 20);
+            this.frequency_in.TabIndex = 9;
+            this.frequency_in.TextChanged += new System.EventHandler(this.validation);
+            // 
+            // gain_in
+            // 
+            this.gain_in.Location = new System.Drawing.Point(516, 38);
+            this.gain_in.Name = "gain_in";
+            this.gain_in.Size = new System.Drawing.Size(45, 20);
+            this.gain_in.TabIndex = 10;
+            this.gain_in.TextChanged += new System.EventHandler(this.validation);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(383, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Amplification";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(513, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "gain";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(383, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Capacitance";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(513, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "uF";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(513, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Hz";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(392, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Frequency";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(66, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "R1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(199, 238);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "R2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(316, 238);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "R3";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(445, 238);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(20, 13);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "C2";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(264, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(109, 142);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 22;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Amplifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 361);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gain_in);
+            this.Controls.Add(this.frequency_in);
+            this.Controls.Add(this.capacitance_in);
+            this.Controls.Add(this.amp_rate_in);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.list_c2);
+            this.Controls.Add(this.list_r3);
+            this.Controls.Add(this.list_r2);
+            this.Controls.Add(this.list_r1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Amplifier";
+            this.Text = "Filter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fitro_FormClosing);
+            this.Load += new System.EventHandler(this.enable);
+            this.TextChanged += new System.EventHandler(this.validation);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox list_r1;
+        private System.Windows.Forms.ListBox list_r2;
+        private System.Windows.Forms.ListBox list_r3;
+        private System.Windows.Forms.ListBox list_c2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox amp_rate_in;
+        private System.Windows.Forms.TextBox capacitance_in;
+        private System.Windows.Forms.TextBox frequency_in;
+        private System.Windows.Forms.TextBox gain_in;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fitro
+{
+    public partial class Amplifier : Form
+    {
+        public Amplifier()
+        {
+            InitializeComponent();
+        }
+        
+        int count = 1;
+        double ampli_value = 0, capacitance1_value = 0, frequency_value = 0, gain_value = 0;
+
+        public void enable()
+        {
+            button1.Enabled=false; button2.Enabled=false;
+
+            try
+            {
+                ampli_value=Convert.ToDouble(amp_rate_in.Text);
+                capacitance1_value=Convert.ToDouble(capacitance_in.Text);
+                frequency_value=Convert.ToDouble(frequency_in.Text);
+                gain_value=Convert.ToDouble(gain_in.Text);
+
+                if (ampli_value !=0 && capacitance1_value!=0 && frequency_value!=0)
+                {
+                    button1.Enabled=true; button2.Enabled=true;
+                }
+            }
+
+            catch { }
+        }
+        private void validation(object sender, EventArgs e)
+        {
+            try {
+                ampli_value=Convert.ToDouble(amp_rate_in.Text);
+                capacitance1_value=Convert.ToDouble(capacitance_in.Text);
+                frequency_value=Convert.ToDouble(frequency_in.Text);
+                gain_value=Convert.ToDouble(gain_in.Text);
+
+                if (ampli_value !=0 && capacitance1_value!=0 && frequency_value!=0)
+                {
+                    button1.Enabled=true; button2.Enabled=true;
+                }
+            }
+
+            catch
+            { }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            count = count % 9 +1;
+            pictureBox1.Image= Image.FromFile(Directory.GetCurrentDirectory() + "\\res0" + Convert.ToString(count)+ ".png");
+
+        }
+
+        private void enable(object sender, EventArgs e)
+        {
+          
+                button1.Enabled=false; button2.Enabled=false;
+
+                try
+                {
+                    ampli_value=Convert.ToDouble(amp_rate_in.Text);
+                    capacitance1_value=Convert.ToDouble(capacitance_in.Text);
+                    frequency_value=Convert.ToDouble(frequency_in.Text);
+                    gain_value=Convert.ToDouble(gain_in.Text);
+
+                    if (ampli_value !=0 && capacitance1_value!=0 && frequency_value!=0)
+                    {
+                        button1.Enabled=true; button2.Enabled=true;
+                    }
+                }
+
+                catch { }
+            
+        }
+
+        private void fitro_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Close this app?", "Are you sure?",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2)==DialogResult.Cancel)
+                e.Cancel=false;
+        }
+
+        private void calcular(object sender, EventArgs e)
+        {
+            double r1, r2, r3, c2;
+            int b;
+            double ampli_gain = ampli_value*gain_value;
+            for (int a = 0; a<5; a++)
+            {
+                b = a +1;
+                r1= Math.Sqrt(2) / (2 * a * 2 * Math.PI * frequency_value * capacitance1_value);
+                r2=ampli_gain*r1;
+                r3=(r2/ampli_gain+1);
+                c2=2*(ampli_gain+1)*capacitance1_value;
+
+                list_r1.Items.Add(b.ToString("#0")+ "....." + r1.ToString("#0.00000"));
+                list_r2.Items.Add(b.ToString("#0")+ "....." + r2.ToString("#0.00000"));
+                list_r3.Items.Add(b.ToString("#0")+ "....." + r3.ToString("#0.00000"));
+                list_c2.Items.Add(b.ToString("#0")+ "....." + c2.ToString("#0.00000"));
+
+
+
+
+
+
+
+            }
+        }
+
+      
+        private void Clean(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Clear all data?", "Are you sure?",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button2)==DialogResult.OK)
+            {
+                amp_rate_in.Clear(); capacitance_in.Clear(); frequency_in.Clear(); gain_in.Clear();
+                list_r1.Items.Clear(); list_r2.Items.Clear(); list_r3.Items.Clear(); list_c2.Items.Clear();
+            }
+           enable();
+        } 
+
+    }
+      
+}
+
+
+   
+
+
